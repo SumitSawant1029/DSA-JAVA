6a221151b62379876a4c9cbe03bb996249c9c8e9
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2024-02-19 11:26:37 +0530

    Sum of Two Numbers Program

---
+++ SumTwoNos.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+class SumTwoNos{
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number:-");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number:-");
+        int b = sc.nextInt();
+
+        System.out.println("Sum Of two Number is :-"+ (a+b));
+    }
+}
