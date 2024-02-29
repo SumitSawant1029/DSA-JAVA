d42c7ffe6e54ef9e508952b96063a40c9d0ef6c3
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2024-02-29 22:48:36 +0530

    Comments done

--- DSAQuestions/Q11_TrappingWater.java
+++ DSAQuestions/Q11_TrappingWater.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Q11_TrappingWater {
 
+    // Finding Minimum Number In two Arrays
     public static int[] Min(int[] a , int[] b) {
         for (int i = 0; i < b.length; i++) {
         if(a[i]<b[i]){
@@ -14,6 +15,7 @@ public class Q11_TrappingWater {
         
     }
 
+    // Reversing the array
     public static int[] Reverse(int[] a){
         int[] b = new int[a.length];
         System.out.println(Arrays.toString(a));
@@ -23,6 +25,7 @@ public class Q11_TrappingWater {
         return b;
      }
 
+    // Finding the max element from the Array from left
     public static int[] maxLeftRight(int[] a) {
         int[] maxLeft = new int[a.length];
         int max = 0;
@@ -44,6 +47,7 @@ public class Q11_TrappingWater {
         return maxLeft;
     }
 
+    // Subtracting the array and omiting the negative Sign
     public static int[] Calculate(int[] a,int[] b) {
         for (int i = 0; i < b.length; i++) {
             if(a[i]-b[i]>0){
@@ -57,17 +61,28 @@ public class Q11_TrappingWater {
         
     }
     public static void main(String[] args) {
+
+        // Depth given 
         int[] a = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+        // Maximum for Depth from Left
         int[] maxLeft = maxLeftRight(a);
+
+        // Reversing so we can use same funtion 
         int[] b = Reverse(a);
+
+        // Maximum for Depth from Left
         int[] maxRight = maxLeftRight(b);
         maxRight = Reverse(maxRight);
+
+        // Calculating the Minimum of two arrays element
         maxLeft = Min(maxLeft,maxRight);
         
+        // Calculating the Water hold index
         int[] depth = Calculate(maxLeft,a);
         System.out.println(Arrays.toString(depth));
 
-        //Adding  the depth
+        //Adding the depth array
         int dep = 0 ; 
         for (int i = 0; i < depth.length; i++) {
             dep = dep + depth[i];
